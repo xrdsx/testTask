c02f344bd99a2ffcfeec1c295ca3c71263bd90a9
Maciej Kaczmarek <ul0253576@edu.uni.lodz.pl>
2022-10-13 21:10:28 +0200

    change name

--- testTask/Models/Reverse.cs
+++ testTask/Models/Reverse.cs
@@ -3,11 +3,11 @@
     public class Reverse
     {
 
-        public static string ShowReverseString(string str)
+        public static string ShowReverseString(string stringToReverse)
         {
-            if ((str == null) || (str.Length <= 1))
-                return str;
-            return ShowReverseString(str.Substring(1)) + str[0];
+            if ((stringToReverse == null) || (stringToReverse.Length <= 1))
+                return stringToReverse;
+            return ShowReverseString(stringToReverse.Substring(1)) + stringToReverse[0];
         }
     }
 }

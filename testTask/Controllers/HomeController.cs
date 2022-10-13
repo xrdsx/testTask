64cbdf7123974dfce16f9cc90ce304dc3d925401
Maciej Kaczmarek <ul0253576@edu.uni.lodz.pl>
2022-10-13 20:58:16 +0200

    finish

---
+++ testTask/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using testTask.Models;
+
+namespace testTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
+
+        [HttpPost]
+        public IActionResult Index(string stringToReverse)
+        {
+            string stringReversed = Reverse.ShowReverseString(stringToReverse);
+            
+            ViewBag.Name = "REVERSED STRING ====> " + stringReversed;
+            return View();
+        }
+
+
+
+
+
+
+    }
+}
---
+++ testTask/Models/Reverse.cs
@@ -0,0 +1,13 @@
+﻿namespace testTask.Models
+{
+    public class Reverse
+    {
+
+        public static string ShowReverseString(string str)
+        {
+            if ((str == null) || (str.Length <= 1))
+                return str;
+            return ShowReverseString(str.Substring(1)) + str[0];
+        }
+    }
+}
